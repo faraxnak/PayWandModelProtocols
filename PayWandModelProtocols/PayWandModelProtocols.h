a6c6e5f0c8622ab996e2db0ead0c54f1b9835488
faraxnak <farid@farids-macbook-pro.local>
2016-12-15 16:13:33 +0330

    Initial Commit

---
+++ PayWandModelProtocols/PayWandModelProtocols.h
@@ -0,0 +1,19 @@
+//
+//  PayWandModelProtocols.h
+//  PayWandModelProtocols
+//
+//  Created by Farid on 12/15/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PayWandModelProtocols.
+FOUNDATION_EXPORT double PayWandModelProtocolsVersionNumber;
+
+//! Project version string for PayWandModelProtocols.
+FOUNDATION_EXPORT const unsigned char PayWandModelProtocolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayWandModelProtocols/PublicHeader.h>
+
+
