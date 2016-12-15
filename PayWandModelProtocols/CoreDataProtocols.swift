1dedd2200a30b431c82e4765388ddeffa25991b8
faraxnak <farid@farids-macbook-pro.local>
2016-12-15 16:17:53 +0330

    created

---
+++ PayWandModelProtocols/CoreDataProtocols.swift
@@ -0,0 +1,19 @@
+//
+//  CoreDataProtocols.swift
+//  PayWandModelProtocols
+//
+//  Created by Farid on 12/15/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+import Foundation
+
+@objc public protocol CountryProtocol {
+    
+    var name: String {get set}
+    var id: NSNumber {get set}
+    var phoneCode: String? {get set}
+    var code : String {get set}
+    var currency : String? {get set}
+    
+}
---
+++ PayWandModelProtocols/ServerDataProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  ServerDataProtocols.swift
+//  PayWandModelProtocols
+//
+//  Created by Farid on 12/15/16.
+//  Copyright © 2016 ParsPay. All rights reserved.
+//
+
+import Foundation
